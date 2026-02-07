9c26be4696fa43aa4c9c54bb2ace64173fbc6d1d
muskan3962 <muskank8445@gmail.com>
2026-02-07 21:56:38 +0530

    Time: 44 ms (30.92%) | Memory: 47.9 MB (30.43%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,26 +1,27 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int[] bCount = new int[n];
-        int[] aCount = new int[n];
-        for(int i=1;i<n;i++){
-            char prevCh = s.charAt(i-1);
-            bCount[i] = bCount[i-1];
-            if(prevCh =='b'){
-                bCount[i]+=1;
+        int aCount = 0;
+        // int[] bCount = new int[n];
+        // int[] aCount = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a') {
+                aCount += 1;
             }
         }
-        for(int i=n-2;i>=0;i--){
-            char prevCh = s.charAt(i+1);
-            aCount[i] = aCount[i+1];
-            if(prevCh =='a'){
-                aCount[i]+=1;
-            }
-        }
+        int seenA = 0;
+        int bCount = 0;
         int minDel = Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
-            minDel = Math.min(minDel,aCount[i]+bCount[i]);
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a') {
+                seenA++;
+            }
+            minDel = Math.min(minDel, (aCount - seenA) + bCount);
+            if (s.charAt(i) == 'b') {
+                bCount += 1;
+            }
         }
+
         return minDel;
     }
 }
