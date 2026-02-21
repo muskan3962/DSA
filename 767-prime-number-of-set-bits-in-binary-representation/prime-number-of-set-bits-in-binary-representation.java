39fbf0d4eeefa7aed2e805d27270c3abc97f34fe
muskan3962 <muskank8445@gmail.com>
2026-02-21 11:47:00 +0530

    Time: 3 ms (91.82%) | Memory: 42.1 MB (90.65%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -2,19 +2,19 @@ class Solution {
     public int countPrimeSetBits(int left, int right) {
         int ans =0;
         for(int i = left;i<=right;i++){
-            int setBit  = numberOfSetBit(i);
+            int setBit  = Integer.bitCount(i);
             if(setBit ==2 || setBit == 3|| setBit == 5|| setBit == 7|| setBit ==11 || setBit ==13 || setBit ==17 || setBit ==19)ans++;
         }
         return ans;
         
     }
 
-    private int numberOfSetBit(int n){
-        int count =0;
-        while(n!=0){
-            n = n&(n-1);
-            count++;
-        }
-        return count;
-    }
+    // private int numberOfSetBit(int n){
+    //     int count =0;
+    //     while(n!=0){
+    //         n = n&(n-1);
+    //         count++;
+    //     }
+    //     return count;
+    // }
 }
