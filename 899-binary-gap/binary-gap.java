3608c42809830d8e62b73f340cac81b95e7e67fd
muskan3962 <muskank8445@gmail.com>
2026-02-22 11:52:17 +0530

    Time: 1 ms (40.87%) | Memory: 42.3 MB (67.94%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,15 +1,14 @@
 class Solution {
     public int binaryGap(int n) {
         String binary = Integer.toBinaryString(n);
+        int prev  = -1;
         int maxGap =0;
-        for(int i =0;i<binary.length();i++){
-            if(binary.charAt(i)=='1'){// geting first 1
-                for(int j = i+1;j<binary.length();j++){
-                    if(binary.charAt(j)=='1'){
-                        maxGap = Math.max(maxGap,j-i);
-                        break;
-                    }
+        for(int i=0;i<binary.length();i++){
+            if(binary.charAt(i)=='1'){
+                if(prev !=-1){
+                    maxGap = Math.max(maxGap,i-prev);
                 }
+                prev =i;
             }
         }
         return maxGap;
