e02919fd735a4e4679eb734a72d76fb1dd096d7f
Muskan Kumari <muskank8445@gmail.com>
2026-04-01 21:23:25 +0530

    Time: 1 ms (99.66%) | Memory: 47.2 MB (81.30%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,10 +1,12 @@
 class Solution {
     public int jump(int[] nums) {
-        int count=0,l=0,r=0;
+        int l = 0,r =0;
+        int count =0;
         while(r<nums.length-1){
             int maxReach =0;
-            for(int i =l;i<=r;i++){
-                maxReach = Math.max(maxReach,nums[i]+i);
+            for(int i=l;i<=r;i++){
+                maxReach = Math.max(maxReach,i+nums[i]);
+
             }
             l = r+1;
             r = maxReach;
