021267667a9812f8fa835ee94e740fa40cb4c64a
Muskan Kumari <muskank8445@gmail.com>
2026-04-23 20:28:50 +0530

    Time: 3 ms (93.29%) | Memory: 45.6 MB (12.06%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,20 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] res = new int[nums1.length];
-        for(int i =0;i<nums1.length;i++){
-            int nge = -1;
-            int j ;
-            for(j =0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j])break;
+        HashMap<Integer,Integer> mp = new HashMap<>();
+       
+       for (int i = 0; i < nums2.length; i++) {
+        int nge = -1;
+
+        for (int j = i + 1; j < nums2.length; j++) {
+            if (nums2[j] > nums2[i]) {
+                nge = nums2[j];
+                break;
             }
-            for(int k = j+1;k<nums2.length;k++){
-                if(nums2[k]>nums2[j]){
-                    nge = nums2[k];
-                    break;
-                }
-            }
-            res[i] = nge;
         }
-        return res;
+
+        mp.put(nums2[i], nge);
+    }
+
+    // Build result for nums1
+    int[] res = new int[nums1.length];
+    for (int i = 0; i < nums1.length; i++) {
+        res[i] = mp.get(nums1[i]);
+    }
+
+    return res;
     }
 }
